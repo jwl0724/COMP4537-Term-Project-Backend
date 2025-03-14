b32198bae5d41109c1ce61330a11699ac26aef7c
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-14 12:15:24 -0700

    Added code to initialize database

--- classes/database/repository.js
+++ classes/database/repository.js
@@ -6,7 +6,29 @@ class Repository {
 
     constructor() {
         this.#connection = implementation.connectToDB();
+        // this.initializeDatabase();
     }
+
+    // async initializeDatabase() {
+    //     try {
+    //         await this.#connection.execute(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`);
+
+    //         // Create Users table if it doesn't exist
+    //         await this.#connection.execute(`
+    //             CREATE TABLE IF NOT EXISTS users (
+    //                 id INT AUTO_INCREMENT PRIMARY KEY,
+    //                 email VARCHAR(255) UNIQUE NOT NULL,
+    //                 password VARCHAR(255) NOT NULL
+    //             )
+    //         `);
+
+    //         console.log(" Database initialized successfully");
+    //     } catch (error) {
+    //         console.error("Error initializing database:", error);
+    //         throw error;
+    //     }
+    // }
+
 
     getUser(email) {
         return implementation.readUser(this.#connection, email);
