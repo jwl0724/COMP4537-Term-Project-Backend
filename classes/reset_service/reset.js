31faf3e282ab3189e396b0ed67848f75198934ac
yuyue-creater <yyu129@my.bcit.ca>
2025-03-14 12:12:10 -0700

    Rest constant added

--- classes/reset_service/reset.js
+++ classes/reset_service/reset.js
@@ -1,5 +1,18 @@
-const reset = function(req, res, db) {
-    // TODO: Need to figure out logistics of this part
-}
+const reset = async function(req, res, db) {
+    try {
+        const { email, newPassword } = req.body;
+        const user = await db.getUser(email);
+        if (!user) return res.status(404).json({ error: "User not found" });
+
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+        await db.writeUser(email, hashedPassword);
+        res.status(200).json({ message: "Password reset successfully" });
+    } catch (error) {
+        console.error("Password reset error:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
 
 exports.reset = reset;
