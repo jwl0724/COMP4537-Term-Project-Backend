b5b5901fa71849dbfad5a17d7e5aa21f7bc06d4e
Jonathan <122506441+jwl0724@users.noreply.github.com>
2025-03-14 09:28:55 -0700

    create reset password service

---
+++ classes/reset_service/reset.js
@@ -0,0 +1,5 @@
+const reset = function(req, res, db) {
+    // TODO: Need to figure out logistics of this part
+}
+
+exports.reset = reset;
--- classes/routeBuilder.js
+++ classes/routeBuilder.js
@@ -1,10 +1,14 @@
 const chat = require("./chat_service/chat");
 const auth = require("./auth_service/auth");
+const reset = require("./auth_service/reset");
 
 function build(app, database) {
     // Auth services
     app.post("/login", (req, res) => auth.login(req, res, database));
     app.post("/signup", (req, res) => auth.signup(req, res, database));
+
+    // Password reset services
+    app.post("/reset", (req, res) => reset.reset(req, res, database));
 
     // Chat services
     app.post("/chat", (req, res) => chat.getChat(req, res));
