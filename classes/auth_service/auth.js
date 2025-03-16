715c40a266afb907bfe6b1213b368a260414c9f0
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-15 19:53:18 -0700

    Removed logs

--- classes/auth_service/auth.js
+++ classes/auth_service/auth.js
@@ -2,15 +2,12 @@ const bcrypt = require("bcryptjs");
 
 const login = async function (req, res, db, next) {
     try {
-        console.log("Login request received:", req.body);
-
         const user = await db.getUser(req.body.email);
         if (!user) throw new Error("User not found");
 
         const match = await bcrypt.compare(req.body.password, user.password);
         if (!match) throw new Error("Invalid credentials");
 
-        console.log(`User logged in with email: ${req.body.email}`);
         res.sendStatus(200);
     } catch (error) {
         next(error);
@@ -23,18 +20,13 @@ const signup = async function (req, res, db, next) {
             throw new Error("Email and password are required");
         }
 
-        console.log("Signup request received:", req.body);
-
         const existingUser = await db.getUser(req.body.email);
         if (existingUser) {
             throw new Error("User already exists");
         }
-
-        console.log("Hashing password...");
         const salt = await bcrypt.genSalt(12);
         const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-        console.log("Inserting user into database...");
         const output = await db.writeUser(req.body.email, hashedPassword);
         if (!output) {
             throw new Error("Error creating user");
