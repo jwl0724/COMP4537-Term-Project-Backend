6765c42a54228e146436e55298a0fefc806282ee
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-15 19:37:23 -0700

    Refactor to use lower case user

--- classes/database/mysql.js
+++ classes/database/mysql.js
@@ -2,37 +2,38 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connectToDB = function () {
-    // Expects to run server with .env file
     const pool = mysql.createPool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         port: process.env.DB_PORT
-    }).promise(); // Enables async/await support
-    return pool;
+    });
+    return pool.promise();
 };
-
 const readUser = async function (pool, email) {
     try {
-        const [rows] = await pool.execute("SELECT * FROM Users WHERE email = ?", [email]);
-        return rows.length ? rows[0] : null; 
+        console.log("Checking user in database:", email);
+        const [rows] = await pool.execute("SELECT * FROM users WHERE email = ?", [email]);
+        return rows.length ? rows[0] : null;
     } catch (error) {
-        console.error("Database read error:", error)
+        console.error("Database read error:", error);
         throw error;
     }
 };
+
 
 const writeUser = async function (pool, email, hashedPassword) {
     try {
-        const [result] = await pool.execute("INSERT INTO Users (email, password) VALUES (?, ?)", [email, hashedPassword]);
+        const [result] = await pool.execute(
+            "INSERT INTO users (email, password) VALUES (?, ?)",
+            [email, hashedPassword]
+        );
         return result;
     } catch (error) {
-        console.error("Database insertion error: ", error);
+        console.error("Database insertion error:", error);
         throw error;
     }
 };
 
-exports.connectToDB = connectToDB;
-exports.readUser = readUser;
-exports.writeUser = writeUser;
+module.exports = { connectToDB, readUser, writeUser };
