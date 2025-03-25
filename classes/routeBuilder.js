071a3d186117042afef8b3ba561990a4f54a794d
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-25 01:16:57 -0700

    Refactor routebuilder to use logApi util

--- classes/routeBuilder.js
+++ classes/routeBuilder.js
@@ -2,35 +2,36 @@ const chat = require("./chat_service/chat");
 const auth = require("./auth_service/auth");
 const reset = require("./reset_service/reset");
 const DataController = require("./data_service/dataController");
+const { logApi } = require("./utils/logApi");
 const { verifyToken } = require("./utils/token");
 
-function build(app, database) {
-    const dataController = new DataController(database);
+function build(app, db) {
+    const dataController = new DataController(db);
     // Auth services
-    app.post("/login", (req, res, next) => auth.login(req, res, database, next));
-    app.post("/signup", (req, res, next) => auth.signup(req, res, database, next));
+    app.post("/login", (req, res, next) => auth.login(req, res, db, next));
+    app.post("/signup", (req, res, next) => auth.signup(req, res, db, next));
     app.post("/logout", (req, res) => auth.logout(req, res))
 
     // Password reset services
-    app.post("/reset", verifyToken, (req, res, next) => reset.reset(req, res, database, next));
+    app.post("/reset", verifyToken, logApi(db), (req, res, next) => reset.reset(req, res, db, next));
 
     // Chat services
-    app.post("/chat", verifyToken, (req, res, next) => chat.getChat(req, res, database, next));
+    app.post("/chat", verifyToken, logApi(db), (req, res, next) => chat.getChat(req, res, db, next));
 
     // forgot-password
     app.post("/forgot-password", async (req, res, next) => {
         try {
-            await reset.reset(req, res, database);
+            await reset.reset(req, res, db);
         } catch (error) {
             next(error);
         }
     });
 
     // Data services
-    app.get("/me", verifyToken, (req, res, next) => dataController.getMe(req, res, database, next));
-    app.get("/get-all-users", verifyToken, (req, res, next) => dataController.getAllUserData(req, res, database, next));
-    app.get("/api-stats", verifyToken, (req, res, next) => dataController.getUserApiStats(req, res, database, next));
-    app.get("/endpoint-stats", verifyToken, (req, res, next) => dataController.getEndpointStats(req, res, database, next));
+    app.get("/me", verifyToken, logApi(db), (req, res, next) => dataController.getMe(req, res, db, next));
+    app.get("/get-all-users", verifyToken, logApi(db), (req, res, next) => dataController.getAllUserData(req, res, db, next));
+    app.get("/api-stats", verifyToken, logApi(db), (req, res, next) => dataController.getUserApiStats(req, res, db, next));
+    app.get("/endpoint-stats", verifyToken, logApi(db), (req, res, next) => dataController.getEndpointStats(req, res, db, next));
 }
 
 exports.build = build;
