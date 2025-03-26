260cbc4d7f6cbe6f249d086f75005118082d13f6
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-25 22:16:12 -0700

    Refactor routerbuilder to be arrow function and use /api/v1 route

--- classes/routeBuilder.js
+++ classes/routeBuilder.js
@@ -1,27 +1,24 @@
+const express = require("express");
 const chat = require("./chat_service/chat");
 const auth = require("./auth_service/auth");
 const reset = require("./reset_service/reset");
 const { clearResources } = require("./utils/cleaner");
-const DataController = require("./data_service/dataController");
 const { logApi } = require("./utils/logApi");
 const { verifyToken } = require("./utils/token");
+const DataController = require("./data_service/dataController");
 
-function build(app, db) {
-    const dataController = new DataController(db);
+const build = (db) => {
+    const router = express.Router();
+    const dc = new DataController(db);
 
-    // Auth services
-    app.post("/login", (req, res, next) => auth.login(req, res, db, next));
-    app.post("/signup", (req, res, next) => auth.signup(req, res, db, next));
-    app.post("/logout", verifyToken, clearResources, (req, res) => auth.logout(req, res));
+    // Auth routes
+    router.post("/login", (req, res, next) => auth.login(req, res, db, next));
+    router.post("/signup", (req, res, next) => auth.signup(req, res, db, next));
+    router.post("/logout", verifyToken, clearResources, (req, res) => auth.logout(req, res));
 
-    // Password reset services
-    app.post("/reset", verifyToken, logApi(db), (req, res, next) => reset.reset(req, res, db, next));
-
-    // Chat services
-    app.post("/chat", verifyToken, logApi(db), (req, res, next) => chat.getChat(req, res, db, next));
-
-    // forgot-password
-    app.post("/forgot-password", async (req, res, next) => {
+    // Password reset
+    router.post("/reset", verifyToken, logApi(db), (req, res, next) => reset.reset(req, res, db, next));
+    router.post("/forgot-password", async (req, res, next) => {
         try {
             await reset.reset(req, res, db);
         } catch (error) {
@@ -29,14 +26,19 @@ function build(app, db) {
         }
     });
 
-    // Data services
-    app.get("/me", verifyToken, logApi(db), (req, res, next) => dataController.getMe(req, res, next));
-    app.get("/get-all-users", verifyToken, logApi(db), (req, res, next) => dataController.getAllUserData(req, res, next));
-    app.get("/api-stats", verifyToken, logApi(db), (req, res, next) => dataController.getApiStats(req, res, next));
-    app.get("/endpoint-stats", verifyToken, logApi(db), (req, res, next) => dataController.getEndpointStats(req, res, next));
-    app.put("/update-api-calls", verifyToken, logApi(db), (req, res, next) => dataController.updateApiCallsLeft(req, res, next));
-    app.put("/update-role", verifyToken, logApi(db), (req, res, next) => dataController.updateRole(req, res, next));
-    app.delete("/delete-user", verifyToken, logApi(db), (req, res, next) => dataController.deleteUser(req, res, next));
-}
+    // Chat
+    router.post("/chat", verifyToken, logApi(db), (req, res, next) => chat.getChat(req, res, db, next));
 
-exports.build = build;
+    // Data
+    router.get("/me", verifyToken, logApi(db), (req, res, next) => dc.getMe(req, res, next));
+    router.get("/get-all-users", verifyToken, logApi(db), (req, res, next) => dc.getAllUserData(req, res, next));
+    router.get("/api-stats", verifyToken, logApi(db), (req, res, next) => dc.getApiStats(req, res, next));
+    router.get("/endpoint-stats", verifyToken, logApi(db), (req, res, next) => dc.getEndpointStats(req, res, next));
+    router.put("/update-api-calls", verifyToken, logApi(db), (req, res, next) => dc.updateApiCallsLeft(req, res, next));
+    router.put("/update-role", verifyToken, logApi(db), (req, res, next) => dc.updateRole(req, res, next));
+    router.delete("/delete-user", verifyToken, logApi(db), (req, res, next) => dc.deleteUser(req, res, next));
+
+    return router;
+};
+
+module.exports = { build };
