19276a9e10d86b64e14c7a18e69638c3f56f7fec
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-30 14:24:49 -0700

    Fix endpoint

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -2,6 +2,7 @@ exports.SPONGEBOB_MODEL = "weight_tq6pwerrbr4mvbjmtyhbsqe6t";
 exports.BASE_CDN_URL = "https://cdn-2.fakeyou.com";
 exports.ALLOWED_ORIGINS = {
     prod: [
+        "https://term-project-metdh.ondigitalocean.app",
         "https://funny-cascaron-801788.netlify.app",
         "https://spongbob-test.netlify.app",
         "https://comp-4537-term-project-frontend-three.vercel.app"
