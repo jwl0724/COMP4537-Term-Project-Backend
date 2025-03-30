c89ad417b9be3279b12e2bc45df1ba5c03b9c848
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-30 13:48:21 -0700

    Allowed !origin

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -2,7 +2,6 @@ exports.SPONGEBOB_MODEL = "weight_tq6pwerrbr4mvbjmtyhbsqe6t";
 exports.BASE_CDN_URL = "https://cdn-2.fakeyou.com";
 exports.ALLOWED_ORIGINS = {
     prod: [
-        "https://spongechat-b2ng7.ondigitalocean.app/",
         "https://funny-cascaron-801788.netlify.app",
         "https://spongbob-test.netlify.app",
         "https://comp-4537-term-project-frontend-three.vercel.app"
