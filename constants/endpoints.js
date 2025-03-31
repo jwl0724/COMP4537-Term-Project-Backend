9743c42f02d946ac674279bce5ea166ceca0ba20
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-03-30 17:47:34 -0700

    Finalize endpoints

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -3,8 +3,6 @@ exports.BASE_CDN_URL = "https://cdn-2.fakeyou.com";
 exports.ALLOWED_ORIGINS = {
     prod: [
         "https://term-project-metdh.ondigitalocean.app",
-        "https://funny-cascaron-801788.netlify.app",
-        "https://spongbob-test.netlify.app",
         "https://comp-4537-term-project-frontend-three.vercel.app"
     ],
 }
